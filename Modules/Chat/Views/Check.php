3b96120ce5321a03955e0db06e4d00462ca08fc6
fcparagp <fc@paragp.ch>
2015-05-29 08:28:13 +0200

    ajout script page check

--- Modules/Chat/Views/Check.php
+++ Modules/Chat/Views/Check.php
@@ -1,3 +1,21 @@
+<script type="text/javascript">
+
+function callBack() {
+	setTimeout(function(){
+		check("<?php echo $rootLang;?>", JSON.stringify(new Object()));
+	}, 1000);
+}
+</script>
+<script src="<?php echo $root;?>/Web/js/check.js"></script>
+
+<script>
+$(document).ready(function() {
+	callBack();
+});
+</script>
+
+
+
 <div>
 	<form method="post" action="check.php">
 		
