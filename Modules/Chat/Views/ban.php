14e289ed31a320050072d75f72527b75a9e18bc2
fcparagp <fc@paragp.ch>
2015-06-30 14:04:27 +0200

    ---

--- Modules/Chat/Views/ban.php
+++ Modules/Chat/Views/ban.php
@@ -4,13 +4,10 @@ $('#fermer').click(function(){
 	
 }
 </script>
-
-<div>		
-	<?php 
-	echo BAN_CHAT;
-	?>			
-	
-<div class="bouton">
-				<button class="btn-warning btn-lg btn" type="button" id="fermer"><?php echo CLOSE;?></button>
-			</div>	
+<div class="divcadre">
+	<div class="textcenter">		
+		<?php 
+		echo BAN_CHAT;
+		?>				
+	</div>
 </div>
