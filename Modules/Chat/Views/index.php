454fd53257df4c4af7e3dea79a32a43157596c81
fcparagp <fc@paragp.ch>
2015-07-01 16:26:43 +0200

    prout prout

--- Modules/Chat/Views/index.php
+++ Modules/Chat/Views/index.php
@@ -34,42 +34,30 @@
 $( document ).ready(function() {	
 	
 	$("#sendfirstform").click(function(){
-
-		function validateEmail(email){
-			var emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-			var valid = emailReg.test(email);
-
-			if(!valid) {
-				$("#mail").css("border-color","red").css("border-style","solid");
-				alertify.error('Veuillez saisir une adresse e-mail correcte');
-		    } else {
-		    	var mail = $('#mail').val();	
-		    }
+		var email = $('#mail').val()
+		if (/^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/.test(email)) {
+			var email = $('#mail').val()
+		} else {
+			$("#mail").css("border-color","red").css("border-style","solid");
+			alertify.error('Veuillez saisir une adresse e-mail correcte');
 		}
-
-		//if( !isValidEmailAddress(emailaddress) ) { 
-		//	var mail = $('#mail').val()
-		//} else {
-		//	$("#mail").css("border-color","red").css("border-style","solid");
-		//	alertify.error('Veuillez saisir une adresse e-mail correcte');
-		//}
 		if($("#service").val() != "") { //attention il faut ralouter un autre champ
 			var service = $('.listederoulante').val();
 		} else {
 			$("#service").css("border-color","red").css("border-style","solid");
 			alertify.error('Veuillez selectionner le département concerné');
 		}
-		if($("#probleme").length >= 100 ) { 
+		if($("#probleme").length > 100 ) { 
 			var probleme = $('#probleme').val();
 		} else {
 			$("#probleme").css("border-color","red").css("border-style","solid");
 			alertify.error("Veuillez décrire votre problème :<br> Au minimum 100 caractères sont requis." );
 		}
-		if ((!isEmailValid(email)) && ($("#service").val()) && ($("#probleme").length >= 100 ) && ($('input[name=agree]').is(':checked'))) {
-			check({"mail": mail, "service": service, "probleme": probleme})
-		} else {
-		    alertify.alert("Veuillez cocher la case qui indique que vous acceptez les conditions d'utilisation");
-		}	
+		//if ((typeof valid !== 'undefined') && ($("#service").val()) && ($("#probleme").length >= 100 ) && ($('input[name=agree]').is(':checked'))) {
+		//	check({"mail": mail, "service": service, "probleme": probleme})
+		//} else {
+		//    alertify.alert("Veuillez cocher la case qui indique que vous acceptez les conditions d'utilisation");
+		//}	
 	});
 });
 
