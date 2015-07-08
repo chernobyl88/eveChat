efac68b4be56c4092acfe93efd225da858dab464
fcparagp <fc@paragp.ch>
2015-07-08 12:21:41 +0200

    chat en cours

--- Modules/Chat/Views/chat.php
+++ Modules/Chat/Views/chat.php
@@ -7,42 +7,60 @@ function post(data,base) {
 		    "url": base+"/Chat/sendMessage.html"
 		})
 }
+
+	function entree(){
+		$('#message').keyup(function(e) {   
+	        if(e.keyCode == 13) { 
+	          if (e.shiftKey == false){
+	              $('#envoyer').click();
+	          }
+	   		}
+	  	});
+	};
+	  
+	function load(base){
+		$.get("<?php echo $rootLang;?>/Chat/loadMessage.html",function(message){
+			alert("message:" + message)
+		});
+	};
 		
 $( document ).ready(function() {
-	  function send() {
-		  $("#envoyer").click(function(){
-			if($("#message").val().length >= 5 ){
-			    var nom = "moi";
-		        var message = $('#message').val();
-		       post({"nom": nom, "message":message},"<?php echo $rootLang;?>");
-	       } else {
-	    	   alertify.error("Si vous le désirez" );
-		   }   
-	    })
-	  }
-	function load(base){
-		$.get(base+"/Chat/loadMessage.html",function(message){
-			alert("message:" + message)
-			})
-	}
 
+	
+	
+	$("#envoyer").click(function(){
+		if($("#message").val().length >= 1 ){
+		    var message = $('#message').val();
+		    post({"message":message},"<?php echo $rootLang;?>");
+		    $("#display").append("<div></div>").append($('#message').val());
+		    $("#message").val('');
+		    $('#message').focus();
+		    
+	    } else {
+	        alertify.error("Entrez un message puis cliquez sur Envoyer" );
+		}   
+	});
+
+	load()
+	
 
 });
+
+
 </script>
 
 <div class="divcadre">
-	<div id="display" class="form-control">
-		
+	<div id="display" class="form-control">	
 	</div>
-
-	<div class="form-group">
-		<textarea placeholder="<?php echo ENTER_MSG;?>" class="form-control" id= "message" ></textarea>
-	</div>
-	
-	<div class="bouton">
-		<button class="btn-warning btn-lg btn" type="button" id="envoyer"><?php echo SEND;?></button>
-	</div>
-	<div class="bouton">
-		<button class="btn-warning btn-lg btn" type="button"><?php echo DECO;?></button>
+	<div class="boitemessage">
+		<div class="form-group">
+			<textarea placeholder="<?php echo ENTER_MSG;?>" class="form-control" id= "message" ></textarea>
+		</div>
+		<div class="bouton">
+			<button class="btn-warning btn-lg btn" id="envoyer" type="button"><?php echo SEND;?></button>
+		</div>
+		<div class="bouton">
+			<button class="btn-warning btn-lg btn" type="button"><?php echo DECO;?></button>
+		</div>
 	</div>
 </div>
